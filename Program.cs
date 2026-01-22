e5c10f74068de6c56daa4e8bd25a4116b4311b45
chicuong2k3 <nguyenchicuongk21@gmail.com>
2026-01-23 02:36:27 +0700

    update Program,cs

--- Program.cs
+++ Program.cs
@@ -2,12 +2,20 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DocumentationWeb;
 using DocumentationWeb.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Configure services in a static method so pre-rendering tooling can discover them.
+ConfigureServices(builder.Services, builder.HostEnvironment);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<BookmarkService>();
-
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, IWebAssemblyHostEnvironment hostEnv)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(hostEnv.BaseAddress) });
+    services.AddScoped<BookmarkService>();
+}
