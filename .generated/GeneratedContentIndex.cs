054543573489a01a3c6b2ba32c3792c8b05ab240
chicuong2k3 <nguyenchicuongk21@gmail.com>
2026-01-29 04:02:29 +0700

    Refactor Blazor WebAssembly startup by adjusting script loading and removing HeadOutlet registration, and update generated page content IDs.

--- .generated/GeneratedContentIndex.cs
+++ .generated/GeneratedContentIndex.cs
@@ -6,7 +6,7 @@ public static partial class GeneratedContentIndex
     {
         new PageModel
         {
-            Id = @"73ca6b18-8b36-4fd9-bf32-a1f5860f2b32",
+            Id = @"52e30b2e-115a-4d13-a873-927727f46cab",
             Title = @"Lời nói đầu",
             Slug = @"/on-the-composition-of-images-signs-and-ideas/foreword",
             Description = @"",
@@ -21,7 +21,7 @@ public static partial class GeneratedContentIndex
         },
         new PageModel
         {
-            Id = @"03146122-39ee-47ea-8d83-71282427b21b",
+            Id = @"f4e8ffa2-257d-455d-9e2e-af2c29c01358",
             Title = @"Chương 2: Đại số tuyến tính (Linear algebra)",
             Slug = @"/mathematics-for-machine-learning/chapter-2",
             Description = @"",
@@ -36,7 +36,7 @@ public static partial class GeneratedContentIndex
         },
         new PageModel
         {
-            Id = @"cda2424c-1a1f-428a-940c-bb551c74ce4b",
+            Id = @"d49d07f8-e595-4da9-a746-9198a71c67d1",
             Title = @"Lời tựa",
             Slug = @"/mathematics-for-machine-learning/foreword",
             Description = @"Vai trò của toán học, tư duy nền tảng, và con đường tiếp cận machine learning một cách bài bản.",
@@ -51,7 +51,7 @@ public static partial class GeneratedContentIndex
         },
         new PageModel
         {
-            Id = @"7cc681a7-6882-4f5b-8bb4-6726c5a1873a",
+            Id = @"c3417b05-f2c9-4d13-8483-0dd425db65ac",
             Title = @"Chương 1: Giới thiệu",
             Slug = @"/mathematics-for-machine-learning/chapter-1",
             Description = @"",
