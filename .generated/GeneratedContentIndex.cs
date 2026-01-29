5648a5624c721c6aed92dbe3562d52a3e64eb117
chicuong2k3 <nguyenchicuongk21@gmail.com>
2026-01-29 07:40:51 +0700

    build: Refine static site generation and build process in CI/CD workflow, updating generated content index.

--- .generated/GeneratedContentIndex.cs
+++ .generated/GeneratedContentIndex.cs
@@ -6,7 +6,7 @@ public static partial class GeneratedContentIndex
     {
         new PageModel
         {
-            Id = @"252057b9-b7ea-44bd-b188-ff1a0b13c8e7",
+            Id = @"b16a2c5c-5c83-437a-b357-10fc0c8cdd25",
             Title = @"Lời nói đầu",
             Slug = @"/on-the-composition-of-images-signs-and-ideas/foreword",
             Description = @"",
@@ -21,7 +21,7 @@ public static partial class GeneratedContentIndex
         },
         new PageModel
         {
-            Id = @"ecd6e803-9a05-437d-837b-b49987d11d7b",
+            Id = @"0a20bb11-883b-4f21-94a2-9ff62b8f59c3",
             Title = @"Chương 2: Đại số tuyến tính (Linear algebra)",
             Slug = @"/mathematics-for-machine-learning/chapter-2",
             Description = @"",
@@ -36,7 +36,7 @@ public static partial class GeneratedContentIndex
         },
         new PageModel
         {
-            Id = @"9aaa2f92-2da4-4371-a32c-7002ace7ef60",
+            Id = @"aea9cfdf-1e55-4d34-84a8-3fe41eeabb33",
             Title = @"Lời tựa",
             Slug = @"/mathematics-for-machine-learning/foreword",
             Description = @"Vai trò của toán học, tư duy nền tảng, và con đường tiếp cận machine learning một cách bài bản.",
@@ -51,7 +51,7 @@ public static partial class GeneratedContentIndex
         },
         new PageModel
         {
-            Id = @"150321b8-b84b-48f7-8b37-165ad7e41970",
+            Id = @"53a0a4d0-1f2e-4b34-a635-f087ab7dabbb",
             Title = @"Chương 1: Giới thiệu",
             Slug = @"/mathematics-for-machine-learning/chapter-1",
             Description = @"",
